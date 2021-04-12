37cf65a512c035af774020b091ab7265c94beacd
Kristopher38 <kristopher38@wp.pl>
2021-04-12 04:10:56 +0200

    Fixed off-by-one as well as some other weird input and rendering bugs in Input widget

--- input.lua
+++ input.lua
@@ -4,7 +4,7 @@ local event = require("event")
 
 local Input = utils.makeClass(function(self, sizex, sizey, posx, posy, placeholder, color, textBgColor, textFgColor)
     self:__initBase(Renderable(sizex, sizey, posx, posy, true))
-    self.placeholder = "" or placeholder
+    self.placeholder = placeholder or ""
     self.text = ""
     self.color = color or 0x000000
     self.textFgColor = textFgColor or 0xFFFFFF
@@ -13,7 +13,8 @@ local Input = utils.makeClass(function(self, sizex, sizey, posx, posy, placehold
     self.blinkState = false
     self.blinkInterval = 0.5
     self.internalHandlers = {
-        onFocus = self.internalOnFocus,
+        onFocus = self.internalOnFocusClick,
+        onClick = self.internalOnFocusClick,
         onBlur = self.internalOnBlur,
         onKeyDown = self.internalOnKeyDown,
     }
@@ -32,7 +33,7 @@ function Input:update()
     self:startDraw()
 
     local gpu = self.gpu
-    if #self.text > 0 then
+    if #self.text > 0 or self.focused then
         gpu.setBackground(self.textBgColor)
         gpu.setForeground(self.textFgColor)
         gpu.set(1, 1, self.text)
@@ -46,7 +47,7 @@ function Input:update()
             if self.curPos < #self.text then
                 gpu.set(self.curPos + 1, 1, string.sub(self.text, self.curPos + 1, self.curPos + 1))
             else
-                gpu.set(self.curPos, 1, " ")
+                gpu.set(self.curPos + 1, 1, " ")
             end
         end
     else
@@ -58,10 +59,17 @@ function Input:update()
     self:endDraw()
 end
 
-function Input:internalOnFocus(evt)
+function Input:forceBlinkOn()
+    if self.focused then
+        event.cancel(self.blinkTimerId)
+    end
     self.blinkTimerId = event.timer(self.blinkInterval, self.blinkTimer, math.huge)
-    self.curPos = math.min(#self.text + 1, evt.localx)
     self.blinkState = true
+end
+
+function Input:internalOnFocusClick(evt)
+    self:forceBlinkOn()
+    self.curPos = math.min(#self.text, evt.localx)
     self.shouldUpdate = true
 end
 
@@ -72,13 +80,16 @@ function Input:internalOnBlur(evt)
 end
 
 function Input:internalOnKeyDown(evt)
+    self:forceBlinkOn()
     if evt.code == 14 then -- backspace
-        self.text = string.sub(self.text, 1, self.curPos - 1) .. string.sub(self.text, self.curPos + 1)
+        self.text = #self.text >= self.curPos and
+            string.sub(self.text, 1, math.max(0, self.curPos - 1)) .. string.sub(self.text, self.curPos + 1) or
+            string.sub(self.text, 1, math.max(0, #self.text - 1))
         self.curPos = math.max(0, self.curPos - 1)
     elseif evt.code == 203 then -- left arrow
         self.curPos = math.max(0, self.curPos - 1)
     elseif evt.code == 205 then -- right arrow
-        self.curPos = math.min(#self.text + 1, self.curPos + 1)
+        self.curPos = math.min(#self.text, self.curPos + 1)
     else
         self.text = string.sub(self.text, 1, self.curPos) .. string.char(evt.char) .. string.sub(self.text, self.curPos + 1)
         self.curPos = self.curPos + 1
